124900bcd7a65f8e95d859fc1c886dddc46873b7
Cruzo007 <csoni.chirag@gmail.com>
2020-07-12 01:48:41 +0530

    Init'ing Version/ Refactoring

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.query( {} ,function (tabs) { // The Query {} was missing here
-    for (var i = 0; i < tabs.length; i++) {
-      chrome.tabs.executeScript(tabs[i].id, {file: "tfjs.js"});
-      chrome.tabs.executeScript(tabs[i].id, {file: "body-pix.js"});
-      console.log("background loaded both")
-    }
-  });
-});
